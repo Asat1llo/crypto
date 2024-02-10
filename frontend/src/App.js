7527bb1aad4252040299da912d6d4f2996bf797a
Asat1lo <asatilloiminjonov57@gmail.com>
2024-02-10 13:21:16 +0500

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ export function fetchCrypto(){
     return new Promise((resolve)=>{
         setTimeout(()=>{
            resolve(cryptoData)
-        },1)
+        },2000)
     })
 }
 export function fetchAssets(){
     return new Promise((resolve)=>{
         setTimeout(()=>{
            resolve(cryptoAssets)
-        },2)
+        },2000)
     })
 }
